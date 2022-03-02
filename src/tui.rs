870b62707a388c508e67a400fc2c7960d1e1dbfd
Samyak S Sarnayak <samyak201@gmail.com>
2022-03-03 00:49:05 +0530

    refactor: move UI code into a separate module
    Fixes #2
    
    Added a new struct `Text`:
    - abstracts formatting and coloring in such a way that length is
      preserved i.e., zero-width characters are not counted in the length
        - uses a new trait `HasLength` which gives a `length()` method to
          get the actual length
        - `HasLength` is also implemented for a slice of Text
    - implemented conversions from String, &str, char
    
    Added a new struct `ToipeTui`:
    - stores the stdout terminal ui (from termion)
    - moved flush and reset_screen to this
    - added methods to display a raw `Text`, a line of text (which can include
      multiple disjoint `Text`), multiple lines (with each line having
      multiple `Text`).
    - resetting terminal on dropping is handled by this
    
    Changed the `Toipe` struct to use `ToipeTui` methods when possible:
    - displaying words at the beginning uses `display_a_line`
    - displaying green/red chars on typing uses `display_raw_text`
    - displaying results uses `display_lines` to abstract away the special
      handling of zero-width characters and the aligning of text
        - The text formatting part looks a little worse, but everything
          seems much cleaner!
    - added TODOs in places where ToipeTui could be further used instead of
      accessing the stdout directly
    
    also added TODOs for docstrings

---
+++ src/tui.rs
@@ -0,0 +1,195 @@
+use std::{
+    fmt::Display,
+    io::{stdout, Stdout, Write},
+};
+
+use termion::{
+    clear,
+    color::{self, Color},
+    cursor,
+    raw::{IntoRawMode, RawTerminal},
+    style, terminal_size,
+};
+
+use crate::ToipeError;
+
+// TODO: document this
+pub trait HasLength {
+    fn length(&self) -> usize;
+}
+
+// TODO: document this
+pub struct Text {
+    text: String,
+    length: usize,
+}
+
+impl Text {
+    pub fn new(text: String) -> Self {
+        let length = text.len();
+        Self { text, length }
+    }
+
+    pub fn text(&self) -> &String {
+        &self.text
+    }
+
+    pub fn with_faint(mut self) -> Self {
+        self.text = format!("{}{}{}", style::Faint, self.text, style::NoFaint);
+        self
+    }
+
+    pub fn with_underline(mut self) -> Self {
+        self.text = format!("{}{}{}", style::Underline, self.text, style::Reset);
+        self
+    }
+
+    pub fn with_color<C>(mut self, color: C) -> Self
+    where
+        C: Color,
+    {
+        self.text = format!(
+            "{}{}{}",
+            color::Fg(color),
+            self.text,
+            color::Fg(color::Reset)
+        );
+        self
+    }
+}
+
+impl HasLength for Text {
+    fn length(&self) -> usize {
+        self.length
+    }
+}
+
+impl HasLength for [Text] {
+    fn length(&self) -> usize {
+        self.iter().map(|t| t.length()).sum()
+    }
+}
+
+impl From<String> for Text {
+    fn from(text: String) -> Self {
+        Self::new(text)
+    }
+}
+
+impl From<&str> for Text {
+    fn from(text: &str) -> Self {
+        Self::new(text.to_string())
+    }
+}
+
+impl From<char> for Text {
+    fn from(c: char) -> Self {
+        Self::new(c.to_string())
+    }
+}
+
+impl Display for Text {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.text)
+    }
+}
+
+/// terminal UI of toipe
+pub struct ToipeTui {
+    // TODO: make this private
+    pub stdout: RawTerminal<Stdout>,
+}
+
+type MaybeError = Result<(), ToipeError>;
+
+// TODO: document these functions
+impl ToipeTui {
+    pub fn new() -> Self {
+        Self {
+            stdout: stdout().into_raw_mode().unwrap(),
+        }
+    }
+
+    // TODO: make this private
+    pub fn flush(&mut self) -> MaybeError {
+        self.stdout.flush()?;
+        Ok(())
+    }
+
+    pub fn reset_screen(&mut self) -> MaybeError {
+        let (sizex, sizey) = terminal_size()?;
+
+        write!(
+            self.stdout,
+            "{}{}{}",
+            clear::All,
+            cursor::Goto(sizex / 2, sizey / 2),
+            cursor::BlinkingBar
+        )?;
+        self.flush()?;
+
+        Ok(())
+    }
+
+    pub fn display_a_line(&mut self, text: &[Text]) -> MaybeError {
+        self.display_a_line_raw(text)?;
+        self.flush()?;
+
+        Ok(())
+    }
+
+    fn display_a_line_raw(&mut self, text: &[Text]) -> MaybeError {
+        let len = text.length() as u16;
+        write!(self.stdout, "{}", cursor::Left(len / 2),)?;
+        for t in text {
+            self.display_raw_text(t)?;
+        }
+        write!(self.stdout, "{}", cursor::Left(len),)?;
+
+        Ok(())
+    }
+
+    pub fn display_lines(&mut self, lines: &[&[Text]]) -> MaybeError {
+        let (sizex, sizey) = terminal_size()?;
+
+        let mut line_no = 0;
+        let line_offset = lines.len() as u16 / 2;
+
+        for line in lines {
+            write!(
+                self.stdout,
+                "{}",
+                cursor::Goto(sizex / 2, sizey / 2 + line_no - line_offset)
+            )?;
+            self.display_a_line_raw(line)?;
+            line_no += 1;
+        }
+        self.flush()?;
+
+        Ok(())
+    }
+
+    pub fn display_raw_text(&mut self, text: &Text) -> MaybeError {
+        write!(self.stdout, "{}", text)?;
+        Ok(())
+    }
+}
+
+impl Drop for ToipeTui {
+    /// Resets terminal.
+    ///
+    /// Clears screen and sets the cursor to a non-blinking block.
+    ///
+    /// TODO: reset cursor to whatever it was before Toipe was started.
+    fn drop(&mut self) {
+        write!(
+            self.stdout,
+            "{}{}{}",
+            clear::All,
+            cursor::SteadyBlock,
+            cursor::Goto(1, 1)
+        )
+        .expect("Could not reset terminal while exiting");
+        self.flush().expect("Could not flush stdout while exiting");
+    }
+}
