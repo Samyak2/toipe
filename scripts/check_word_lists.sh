5c44181f72e0f7632d2551ceb3d8e10f4d08c8d9
Krithic Kumar <krithickumarub@protonmail.com>
2022-04-18 20:03:36 +0530

    fix: export LOCALE before sorting

--- scripts/check_word_lists.sh
+++ scripts/check_word_lists.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 for f in `ls src/word_lists/`
 do
     echo ">>>>>>>> Checking file $f"
-    sort -c -d "src/word_lists/$f"
+    LC_COLLATE=en_US.UTF-8 sort -c -d "src/word_lists/$f"
     grep -iv "^[a-z]*$" "src/word_lists/$f" && echo "non-alphabetic characters present in one of the word lists." && exit 1
     echo "<<<<<<<<"
 done
-
--- scripts/sort_word_lists.sh
+++ scripts/sort_word_lists.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 for f in `ls src/word_lists/`
 do
-    sort -d -o "src/word_lists/$f" "src/word_lists/$f"
+    LC_COLLATE=en_US.UTF-8 sort -d -o "src/word_lists/$f" "src/word_lists/$f"
 done
