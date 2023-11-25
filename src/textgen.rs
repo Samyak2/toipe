e302c28b7910df2e8bfa73421c05bdf9163a5af6
Samyak S Sarnayak <samyak201@gmail.com>
2023-11-25 20:08:37 +0530

    chore: clippy

--- src/textgen.rs
+++ src/textgen.rs
@@ -75,7 +75,7 @@ impl<T: Seek + io::Read> RawWordSelector<T> {
         let mut buffer = String::new();
 
         fn is_letter(char: u8) -> bool {
-            (b'a'..=b'z').contains(&char)
+            char.is_ascii_lowercase()
         }
 
         loop {
@@ -230,7 +230,6 @@ pub trait WordSelector {
     /// Returns a [`Vec`] containing `num_words` words.
     fn new_words(&mut self, num_words: usize) -> Result<Vec<String>, io::Error> {
         (0..num_words)
-            .into_iter()
             .map(|_| self.new_word())
             .collect()
     }
