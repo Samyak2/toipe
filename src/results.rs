c15f8f5d2112fa16b2379bcd534c27f1d67fae24
Samyak S Sarnayak <samyak201@gmail.com>
2024-02-04 20:14:35 +0530

    test: add tests for accuracy

--- src/results.rs
+++ src/results.rs
@@ -51,3 +51,79 @@ impl ToipeResults {
             / (self.duration().as_secs_f64() / 60.0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_ulps_eq;
+
+    #[test]
+    fn sanity() {
+        let started_at = Instant::now();
+        let ended_at = started_at + Duration::new(10, 0);
+        let results = ToipeResults {
+            total_words: 0,
+            total_chars_typed: 100,
+            total_chars_in_text: 120,
+            total_char_errors: 10,
+            final_chars_typed_correctly: 80,
+            final_uncorrected_errors: 2,
+            started_at,
+            ended_at,
+        };
+
+        assert_eq!(results.duration(), Duration::new(10, 0));
+
+        assert_ulps_eq!(results.accuracy(), 0.9, max_ulps = 1);
+        assert_ulps_eq!(results.wpm(), 84.0, max_ulps = 1);
+    }
+
+    #[test]
+    fn accuracy() {
+        fn get_toipe_results(total_chars_typed: usize, total_char_errors: usize) -> ToipeResults {
+            ToipeResults {
+                total_words: 0,
+                total_chars_typed,
+                total_chars_in_text: 0,
+                total_char_errors,
+                final_chars_typed_correctly: 0,
+                final_uncorrected_errors: 0,
+                started_at: Instant::now(),
+                ended_at: Instant::now(),
+            }
+        }
+
+        let max_ulps = 1;
+
+        // no errors
+        assert_ulps_eq!(
+            get_toipe_results(100, 0).accuracy(),
+            1.0,
+            max_ulps = max_ulps
+        );
+        // nothing typed
+        assert_ulps_eq!(
+            get_toipe_results(0, 0).accuracy(),
+            0.0,
+            max_ulps = max_ulps
+        );
+        // all wrong
+        assert_ulps_eq!(
+            get_toipe_results(100, 100).accuracy(),
+            0.0,
+            max_ulps = max_ulps
+        );
+        // half correct
+        assert_ulps_eq!(
+            get_toipe_results(100, 50).accuracy(),
+            0.5,
+            max_ulps = max_ulps
+        );
+        // more errors than correct
+        assert_ulps_eq!(
+            get_toipe_results(100, 150).accuracy(),
+            -0.5,
+            max_ulps = max_ulps
+        );
+    }
+}
